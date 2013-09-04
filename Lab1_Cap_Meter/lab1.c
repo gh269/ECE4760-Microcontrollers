ef8cb469918c076838eaaa61e9e8c9a266be0429
Gary He <gh269@cornell.edu>
2013-09-04 14:17:44 -0400

    Added lcd refresh and led blink functions

--- Lab1_Cap_Meter/lab1.c
+++ Lab1_Cap_Meter/lab1.c
@@ -4,6 +4,15 @@ Tasks to complete:
 The LCD should be updated every 200 mSec or so.
 An LED should blink about 1/second.
 The capacitance should be measured as quickly as possible as described above.
+
+	Your program will have to (in time order):
+
+	Set PortB3 to an input.
+	Drive PortB2 to zero by making it an output and wait long enough to discharge the capacitor through 100 ohms. Clearly, to dischage to zero volts with 1% accuracy, R2>100*(100ohms).
+	Convert PortB2 to an input and start a timer. The capacitor will start to charge toward Vcc.
+	Detect when the voltage at PortB2 is greater than than the voltage at PortB3. That is, you will have to record when the comparator changes state. You could do this by polling the ACO bit of the ACSR and stopping the clock when ACO changes state, but a much better way to do it is to use the timer1 input capture function set up to be triggered by the comparator. Using input capture gives better timing accuracy and more dynamic range.
+	Repeat
+
 The range of capacitances to be measured is 1 nf to 100 nf.
 The program should detect whether a capacitance is present or not and display an appropriate message if no capacitor is present.
 If present, format the capacitance as an ASCII number and prints the message C = xxx nf to the LCD
@@ -15,14 +24,18 @@ If present, format the capacitance as an ASCII number and prints the message C =
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <util/delay.h> // needed for lcd_lib
+#include "lcd_lib.h"
 
 #include "uart.h"
 
 #define LED_BLINK_PERIOD 1000
 
 //LED Bits
-
 #define ONBOARD_LED 0x04; //LED is on D.2
 
 //Timer TCCR0B Bits
@@ -30,15 +43,29 @@ If present, format the capacitance as an ASCII number and prints the message C =
 #define T0B_CS01 2
 #define T0B_CS00 1
 
+//LCD variables
+#define LCD_REFRESH_RATE 200
+
+const int8_t LCD_initialize[] PROGMEM = "LCD Initialized\0";
+const int8_t LCD_line[] PROGMEM = "line 1\0";
+const int8_t LCD_number[] PROGMEM = "Number=\0";
+int8_t lcd_buffer[17];	// LCD display buffer
+uint16_t count;			// a number to display on the LCD  
+uint8_t anipos, dir;	// move a character around  
+
+//time counter for LED blinking
 volatile unsigned int led_time_count;
+//time counter for LCD refresh
+volatile unsigned int lcd_time_count;
 
 
 //1 ms timebase register
 //Blinks LED 1/second
 ISR (TIMER0_COMPA_vect){
-	if( led_time_count == (LED_BLINK_PERIOD / 2) ){
-		toggle_led();
-	}
+	if( led_time_count > 0)
+		--led_time_count;
+	if( lcd_time_count > 0)
+		--lcd_time_count;
 
 }
 
@@ -64,6 +91,35 @@ void init_timer0(void){
 	TCCR0A = (1 << WGM01);
 }
 
+// LCD setup
+void init_lcd(void){
+	LCDinit();	//initialize the display
+	LCDcursorOFF();
+	LCDclr();				//clear the display
+	LCDGotoXY(0,0);
+	CopyStringtoLCD(LCD_initialize, 0, 0);
+}
+
+//**********************************************************
+// writes contents of lcd_buffer to LCD every 200 mSec
+// 
+void refresh_lcd(void){
+  // increment time counter and format string 
+  sprintf(lcd_buffer,"%-i",count++);	                 
+  LCDGotoXY(7, 0);
+  	// display the count 
+  LCDstring(lcd_buffer, strlen(lcd_buffer));	
+      	
+  // now move a char left and right
+  LCDGotoXY(anipos,1);	   //second line
+  LCDsendChar(' '); 
+      	
+  if (anipos>=7) dir=-1;   // check boundaries
+  if (anipos<=0 ) dir=1;
+  anipos=anipos+dir;
+  LCDGotoXY(anipos,1);	   //second line
+  LCDsendChar('o');
+}
 
 void initialize(void){
 	led_time_count = 0;
@@ -74,10 +130,21 @@ void initialize(void){
 
 	//Enable LED Port
 	DDRD |= ONBOARD_LED; //turn the LED to an output
-	PORTD = 0; //turn off the LED 
+	PORTD = 0; //turn off LED 
 
 
 
 	sei();
+}
+
+int main(void){
+	if( led_time_count == 0){
+		led_time_count = LED_BLINK_PERIOD / 2;
+		toggle_led();
+	}
+	if( lcd_time_count == 0){
+		lcd_time_count = LCD_REFRESH_RATE;
+		refresh_lcd();
+	}
 }
 
