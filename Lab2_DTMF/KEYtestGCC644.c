92f179c06382f870b8b9a7202c4f17cb473a6477
Alex Maass <am838@cornell.edu>
2013-09-23 17:48:00 -0400

    Added debouncing logic.

--- Lab2_DTMF/KEYtestGCC644.c
+++ Lab2_DTMF/KEYtestGCC644.c
@@ -15,6 +15,19 @@
 #define PORTDATA PORTA
 #define PORTIN PINA
 
+//timeout values for each task
+#define t1 200  
+#define t2 30
+#define t3 30 
+volatile unsigned char time1, time2, time3;	//timeout counters 
+unsigned char PushFlag;		//message indicating a button push 
+unsigned char PushState;	//state machine  
+//State machine state names
+#define NoPush 1 
+#define MaybePush 2
+#define Pushed 3
+#define MaybeNoPush 4
+
 // The raw keyscan
 unsigned char key ;   
 // The decoded button number
@@ -31,6 +44,106 @@ unsigned char keytbl[16] = {0x77, 0x7b, 0x7d, 0x7e,
 // putchar and getchar are in uart.c
 FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
+//**********************************************************
+//timer 0 comare match ISR
+ISR (TIMER0_COMPA_vect) {
+  //Decrement the three times if they are not already zero
+  if (time1>0)	--time1;
+  if (time2>0) 	--time2;
+}
+
+//**********************************************************          
+//Task subroutines
+//Task 1
+void task1(void) {
+  time1=t1;  //reset the task timer
+  if (PushFlag) PushFlag = 0;
+}  
+ 
+//******************************* 
+//Task 2  
+void task2(void) {
+	time2=t2;  //reset the task timer
+
+	//get lower nibble
+	PORTDIR = 0x0f;
+	PORTDATA = 0xf0; 
+	_delay_us(5);
+	key = PORTIN;
+  
+	//get upper nibble
+	PORTDIR = 0xf0;
+	PORTDATA = 0x0f; 
+	_delay_us(5);
+	key = key | PORTIN;
+  
+	//find matching keycode in keytbl
+	if (key != 0xff) {
+	  for (butnum=0; butnum<maxkeys; butnum++) {   
+	  	if (keytbl[butnum]==key)  break;   
+	  }
+
+	  if (butnum==maxkeys) butnum=0;
+	  else butnum++;	   //adjust by one to make range 1-16
+	}
+	else butnum=0;
+	
+	// Switching in the finite state machine.
+	if (butnum != 0) { // Possible button push
+		PushFlag = 1;
+		fprintf(stdout, "%d\n\r",butnum);
+	}
+
+	  switch (PushState) {
+     case NoPush: 
+        if (butnum != 0) PushState=MaybePush;
+        else PushState=NoPush;
+        break;
+     case MaybePush:
+        if (butnum != 0) {
+           PushState=Pushed;   
+           PushFlag=1;
+        }
+        else PushState=NoPush;
+        break;
+     case Pushed:  
+        if (butnum != 0) PushState=Pushed; 
+        else PushState=MaybeNoPush;    
+        break;
+     case MaybeNoPush:
+        if (butnum != 0) PushState=Pushed; 
+        else {
+           PushState=NoPush;
+           PushFlag=0;
+        }    
+        break;
+  }
+} 
+ 
+//Initialization used for the timer interrupts for debouncing
+int initialize(void) {
+	//set up timer 0 for 1 mSec timebase 
+	OCR0A = 249;  		//set the compare re to 250 time ticks
+	TIMSK0= (1<<OCIE0A);	//turn on timer 0 cmp match ISR 
+	//set prescalar to divide by 64 
+	TCCR0B= 3; //0b00000011;	
+	// turn on clear-on-match
+	TCCR0A= (1<<WGM01) ;
+
+	//init the task timers
+	time1=t1;
+	time2=t2;  
+
+	//init the  message
+	//for no button push
+	PushFlag = 0;
+	//init the state machine
+	PushState = NoPush;
+
+	//crank up the ISRs
+	sei() ;
+}
+
 int main(void) {
 	//DDRD=0x02;	// PORT D is an input except for UART on D.1
   // Init port B to show keyboard result
@@ -45,30 +158,11 @@ int main(void) {
 
   //endless loop to read keyboard
   while(1) {
-	//get lower nibble
-  	PORTDIR = 0x0f;
-  	PORTDATA = 0xf0; 
-  	_delay_us(5);
-  	key = PORTIN;
-  	  
-  	//get upper nibble
-  	PORTDIR = 0xf0;
-  	PORTDATA = 0x0f; 
-  	_delay_us(5);
-  	key = key | PORTIN;
-  	  
-  	//find matching keycode in keytbl
-  	if (key != 0xff) {
-  	  for (butnum=0; butnum<maxkeys; butnum++) {   
-  	  	if (keytbl[butnum]==key)  break;   
-  	  }
-
-  	  if (butnum==maxkeys) butnum=0;
-  	  else butnum++;	   //adjust by one to make range 1-16
-  	}
-  	else butnum=0;
-
-    // init the DDS phase increment
+	//Used for debouncing
+	if (time1==0)	task1();
+    if (time2==0) 	task2();
+  	
+	// init the DDS phase increment
     // for a 32-bit DDS accumulator, running at 16e6/256 Hz:
     // increment = 2^32*256*Fout/16e6 = 68719 * Fout
     // Fout=1000 Hz, increment= 68719000 
@@ -110,11 +204,6 @@ int main(void) {
 		default:
 			break;
 	}
-
-  	if (butnum != 0){ 
-		fprintf(stdout, "%d\n\r",butnum);
+ 	
   	}
-	//PORTB = ~butnum ;
-  	
-  	} // end while
-  }   //end main
+  }
