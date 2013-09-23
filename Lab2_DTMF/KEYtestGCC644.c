419fea056be5a80fdbd7b8e21bca00b04d215a76
Alex Maass <am838@cornell.edu>
2013-09-23 11:45:53 -0400

    Removed begins and ends.

--- Lab2_DTMF/KEYtestGCC644.c
+++ Lab2_DTMF/KEYtestGCC644.c
@@ -15,20 +15,12 @@
 #define PORTDATA PORTA
 #define PORTIN PINA
 
-#define begin {
-#define end }
-
 // The raw keyscan
 unsigned char key ;   
 // The decoded button number
 unsigned char butnum ;
 
 //key pad scan table
-/*unsigned char keytbl[16]={0xee, 0xed, 0xeb, 0xe7, 
-						  0xde, 0xdd, 0xdb, 0xd7, 
-						  0xbe, 0xbd, 0xbb, 0xb7, 
-						  0x7e, 0x7d, 0x7b, 0x77};
-*/
 unsigned char keytbl[16] = {0x77, 0x7b, 0x7d, 0x7e,
 							0xb7, 0xbb, 0xbd, 0xbe,
 							0xd7, 0xdb, 0xdd, 0xde,
@@ -39,8 +31,7 @@ unsigned char keytbl[16] = {0x77, 0x7b, 0x7d, 0x7e,
 // putchar and getchar are in uart.c
 FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
-int main(void)
-begin
+int main(void) {
 	//DDRD=0x02;	// PORT D is an input except for UART on D.1
   // Init port B to show keyboard result
   DDRB = 0xff;
@@ -51,18 +42,9 @@ begin
   uart_init();
   stdout = stdin = stderr = &uart_str;
   fprintf(stdout, "Starting...\n\r");
-  
-  	// find out if eeprom has EVER been written
-  /*
-  if (eeprom_read_byte((uint8_t*)eeprom_true) != 'T')
-  begin // if not, write some data and the "written" flag
-  	eeprom_write_word((uint16_t*)eeprom_data,0);
-	eeprom_write_byte((uint8_t*)eeprom_true,'T');
-  end   
-	*/
+
   //endless loop to read keyboard
-  while(1)
-  begin 
+  while(1) {
 	//get lower nibble
   	PORTDIR = 0x0f;
   	PORTDATA = 0xf0; 
@@ -76,16 +58,14 @@ begin
   	key = key | PORTIN;
   	  
   	//find matching keycode in keytbl
-  	if (key != 0xff)
-  	begin   
-  	  for (butnum=0; butnum<maxkeys; butnum++)
-  	  begin   
+  	if (key != 0xff) {
+  	  for (butnum=0; butnum<maxkeys; butnum++) {   
   	  	if (keytbl[butnum]==key)  break;   
-  	  end
+  	  }
 
   	  if (butnum==maxkeys) butnum=0;
   	  else butnum++;	   //adjust by one to make range 1-16
-  	end  
+  	}
   	else butnum=0;
 
     // init the DDS phase increment
@@ -136,5 +116,5 @@ begin
   	}
 	//PORTB = ~butnum ;
   	
-  	end // end while
-  end   //end main
+  	} // end while
+  }   //end main
