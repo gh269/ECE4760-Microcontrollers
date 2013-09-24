815615df24698fc9f443260fdac8f3a6267ef62a
Gary He <gh269@cornell.edu>
2013-09-24 01:24:44 -0400

    fixed circular define of WGM00 and WGM01

--- Lab2_DTMF/atmega1284p.h
+++ Lab2_DTMF/atmega1284p.h
@@ -1,7 +1,6 @@
 #ifndef __4760_1284P_H__
 #define __4760_1284P_H__
 
-#define 
 
 #define CS00 1
 #define TIMER0_OVERFLOW_INTERRUPT_ENABLE (1 << TOIE0)
@@ -9,8 +8,8 @@
 #define COMPARE_MATCH_OUTPUT_A0 (1 << COM0A0)
 #define COMPARE_MATCH_OUTPUT_A1 (1 << COM0A1)
 
-#define WGM00 (1<<WGM00)
-#define WGM01 (1 << WGM01)
+#define WAVE_GEN_M00 (1<<WGM00)
+#define WAVE_GEN_M01 (1 << WGM01)
 
 #define TRUE 1
 #define FALSE 0
