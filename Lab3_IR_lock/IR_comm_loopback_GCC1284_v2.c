a099fa91bf25d9b83ee16fa0dab325c56445d4e7
Alex Maass <am838@cornell.edu>
2013-10-08 02:51:43 -0400

    Quick review. Code now compiles. Needs testing.

--- Lab3_IR_lock/IR_comm_loopback_GCC1284_v2.c
+++ Lab3_IR_lock/IR_comm_loopback_GCC1284_v2.c
@@ -95,8 +95,7 @@ volatile char ir_tx_ready, ir_rx_ready ;
 // sends ir character (if ready)
 // receives ir char (if valid)
 // builds ir receive buffer
-ISR (TIMER0_COMPA_vect) 
-begin  
+ISR (TIMER0_COMPA_vect) {
 	unsigned char c ;
 	    
   	//Decrement the time if not already zero
@@ -143,31 +142,7 @@ begin
 		}
 
 	} //}if c>0
-	
-end  
-
-//*********************************************************
-// UART 1 interface to use with PC
-// --- putchar ---
-// uses uart 1
-void putchar_uart1(char c)
-{
-  //if (c == '\n')
-  //  uart_putchar('\r', stream);
-  loop_until_bit_is_set(UCSR1A, UDRE1);
-  UDR1 = c;
-}
-
-// -- getchar ---
-// users uart 1 -- very basic input
-char getchar_uart1(void)
-{
-  uint8_t c;
-  loop_until_bit_is_set(UCSR1A, RXC1);
-  return c = UDR1;
-}
-
-FILE uart1 = FDEV_SETUP_STREAM(putchar_uart1, getchar_uart1, _FDEV_SETUP_RW);
+} 
 
 //**********************************************************
 // IR send
@@ -239,105 +214,6 @@ char ir_rec_packet(char tx_id, char ir_data[])
 	return rx_status ;
 }
 
-
-//**********************************************************
-// IR Receive ANY transmitter
-// 
-// returns tx_id (>0x30) if payload is valid and returns payload
-// return==1 means no data; 2 means buffer overrun; 4 means bad checksum
-char ir_rec_any_packet(char ir_data[])
-{
-	char rx_status = 0;
-	char ir_rx_ck_sum, ir_rx_ck_sum_ref;
-	char i ;
-	char buf_len ;
-
-	buf_len = (char)strlen(ir_rx_buffer) ;
-	if (buf_len >= buffer_size) buf_len = buffer_size-1;
-
-	// error check
-	if (ir_rx_ready == 2) return 2 ; // buffer overrun
-	if (ir_rx_ready == 0) return 1 ; // no data -- timeout
-	if (ir_rx_ready != 1) {rx_status = 1; return rx_status;} // invalid data
-	// if (ir_rx_buffer[0] != tx_id) return 3 ; // bad transmitter id
-
-	// compute receive checksum
-	ir_rx_ck_sum = 0 ;
-	for (i=1; i<buf_len-2; i++)
-		ir_rx_ck_sum ^= ir_rx_buffer[i] ;
-	
-	ir_rx_ck_sum_ref = (ir_rx_buffer[buf_len-2] & 0x0f) +
-		 (ir_rx_buffer[buf_len-1]<<4) ;
-
-	if (ir_rx_ck_sum_ref != ir_rx_ck_sum) return 4 ; // bad check sum
-
-	// set up the valid return stuff
-	// if no errors, then return transmitter ID
-	if (rx_status == 0) rx_status = ir_rx_buffer[0] ;
-	ir_rx_buffer[0] = ' '; // strip the transmit id from string
-	ir_rx_buffer[buf_len-2] = 0x00; // strip the check sum and trailer
-	ir_rx_ready = 0 ;
-	//trim initial space using ir_rx_buffer+1
-	strlcpy(ir_data, ir_rx_buffer+1, buffer_size) ;
-
-	return rx_status ;
-}
-
-//**********************************************************          
-// send something
-void send_task(void) 
-{  
-	char tx_id = 'T'; // note: this is a quoted char
-	char ir_tx_data[buffer_size] ;
-
-	// transmit
-	sprintf(ir_tx_data,"%ld, %d, %d", time, task_count, error_count); //
-	// send payload from tx_id
-	ir_send_packet(tx_id, ir_tx_data);
-}  	
-
-//**********************************************************          
-// receive it back
-void recv_task(void) 
-{  
-	char rx_id = 'T';  // note: this is a quoted char
-	char rx_status = 0 ;
-	char ir_rx_data[buffer_size]  ;
-	
-	// receive
-	rx_status = ir_rec_packet(rx_id, ir_rx_data) ;
-	if (!rx_status) {
-		//ir_rx_data[6] = 0;
-		fprintf(&uart1, "PAYLOAD=%s\n\r", ir_rx_data);
-	}
-	else {
-		fprintf(&uart1, "%d%s\n\r", rx_status, ir_rx_data);
-		error_count++ ;
-	}
-	task_count++;
-}  	
-
-//**********************************************************          
-// receive it back
-void recv_any_task(void) 
-{  
-//	char rx_id = '1';  // note: this is a quoted char
-	char rx_status = 0 ;
-	char ir_rx_data[buffer_size]  ;
-	
-	// receive
-	rx_status = ir_rec_any_packet(ir_rx_data) ;
-	if (rx_status>=0x30) {
-		//ir_rx_data[6] = 0;
-		fprintf(&uart1, "TX_ID=%c\n\r", rx_status); // transmitter number
-		fprintf(&uart1, "PAYLOAD=%s\n\r", ir_rx_data);// payload
-	}
-	else {
-		fprintf(&uart1, "%d%s\n\r", rx_status, ir_rx_data);
-		error_count++ ;
-	}
-	task_count++;
-}  	
 //********************************************************** 
 //Set it all up
 void initialize(void)
@@ -366,22 +242,10 @@ void initialize(void)
   // PORT D.7 is OC2A
   DDRD = (1<<PIND7) ;	
   //********************
-  
-  //init the task timers
-  time1=t1;
-  time2=t2;
-  
-  //********************  
-
   //init UART0 for IR comm
   UBRR0L = (F_CPU / (16UL * IR_UART_BAUD)) - 1;
   UCSR0B = _BV(TXEN0) | _BV(RXEN0); /* tx/rx enable */
   UCSR0C = (1<<UCSZ01) | (1<<USBS0) ; // 7 bit | 2 stop bits
-
-  //init UART1 for PC comm
-  UBRR1L = (F_CPU / (16UL * PC_UART_BAUD)) - 1;
-  UCSR1B = _BV(TXEN1) ; //| _BV(RXEN1); /* tx/rx enable */
-  fprintf(&uart1,"\n\r...Starting IR comm ...\n\r");
   
   //********************
   //crank up the ISRs
--- Lab3_IR_lock/mainTRT_1284.c
+++ Lab3_IR_lock/mainTRT_1284.c
@@ -3,6 +3,7 @@
 #include "IR_comm_loopback_GCC1284_v2.c"
 #include <util/delay.h>
 #include <stdio.h>
+#include <stdlib.h> 
 
 // serial communication library
 // Don't mess with the semaphores
@@ -14,8 +15,9 @@
 // putchar and getchar are in uart.c
 FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
-// allow task2 to control task1
-#define SEM_TASK1_WAIT 3
+// input arguments to each thread
+// not actually used in this example
+int args[2] ;
 
 // two semaphores to protect message --
 // sender must wait until message is received 
@@ -27,36 +29,18 @@ FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 // semaphore to protect shared variable
 #define SEM_SHARED 7
 
-// the usual
-#define begin {
-#define end }
-
-// --- Blink LEDs and run uart ---------------------------------
-// input arguments to each thread
-// not actually used in this example
-int args[4] ;
-
-// a value passed between task3 and task4
-uint16_t Message_vin ;
-
-// shared variable to count number of total task invocations
-uint32_t task_count ;
-
-// switches
-#define STATESW 0x01
-#define FUNCTIONSW 0x02
-#define AUTHSW 0x04
-#define LOCKSW 0x08
-#define UNLOCKSW 0x10
+// binary states
+#define FALSE 0
+#define TRUE 1
 
 // switch states
 unsigned char mode;
 #define TEST 0
 #define NORMAL 1
 unsigned char function; 
-#define LOCK 0
-#define KEY 1
-#define BOTH 2
+#define fLOCK 0
+#define fKEY 1
+#define fBOTH 2
 unsigned char authorization;
 #define AUTHORIZED '>'
 #define UNAUTHORIZED '<'
@@ -67,21 +51,19 @@ unsigned char authorization;
 #define CHALLENGE 2
 #define RESPONSE 3
 
-// binary states
-#define FALSE 0
-#define TRUE 1
-
+#define LOCK 0
+#define UNLOCK 1
+//state of the lock (unlocked or locked_)
+unsigned char state;
 //state variable for the IR_key task
 unsigned char key_state;
 //state variable for the IR_lock task
 unsigned char lock_state;
 //task to transmit
 unsigned char lock_command;
-#define LOCK_SIG 0
-#define UNLOCK_SIG 1
 
 //recieved time used to prevent man in the middle attacks
-volatile char challenge_time_string[buffer_size]; 
+char* challenge_time_string; 
 
 // Synchronized task to read the switches
 void read_switches() {
@@ -91,20 +73,21 @@ void read_switches() {
 	if (~PINB & 0x01) {
 		mode = NORMAL;
 		// Read switch 1 to select key or lock functionality
-		if (~PINB & 0x02) function = KEY;
-		else function = LOCK;
+		if (~PINB & 0x02) function = fKEY;
+		else function = fLOCK;
 	}
 	else {
 		mode = TEST;
-		function = BOTH;
+		function = fBOTH;
 	}
 		// Read switch 2 to select authorized or unauthorized
 	authorization = ~PINB & 0x04 ? AUTHORIZED : UNAUTHORIZED;
+	fprintf(stdout, "%c %c %c", mode, function, authorization);
 	trtSignal(SEM_SHARED);
 }
 
 // --- define task 1  ----------------------------------------
-void IR_key(void* args) {
+void IR_key() {
 	uint32_t rel, dead;
 	char ir_tx_data[buffer_size];
 	char ir_rx_data[buffer_size];
@@ -114,15 +97,16 @@ void IR_key(void* args) {
 	read_switches();
 	// FSM for IR_key
 	while (TRUE) {
+		/*
 		// Check that the function is either KEY or BOTH
-		if (function == KEY | function == BOTH ) {
+		if ((function == fKEY) | (function == fBOTH )) {
 			switch (key_state) {
 				case IDLE:
 					// Check to see if a button is pressed
-					// 0x08 is to unlock & 0x10 is to lock
-					if (~PINB & 0x08 | ~PINB & 0x10) {
+					// 0x08 is to lock & 0x10 is to unlock
+					if ((~PINB & 0x08) | (~PINB & 0x10)) {
 						key_state = REQUEST;
-						lock_command = ~PINB & 0x08 ? UNLOCK_SIG : LOCK_SIG;
+						lock_command = ~PINB & 0x08 ? LOCK : UNLOCK;
 					}
 					break;
 				case REQUEST:
@@ -148,35 +132,40 @@ void IR_key(void* args) {
 					ir_send_packet('K', ir_tx_data);
 					trtSignal(SEM_TX_WAIT);
 					key_state = IDLE;
-				case DEFAULT:
+				default:
 					// Something is wrong. Recover to IDLE. 
 					key_state = IDLE;
 			}
 		}
+		*/
 		// Sleep
 	    rel = trtCurrentTime() + SECONDS2TICKS(0.1);
-	    dead = trtCurrentTime() + SECONDS2TICKS(0.1);
+	    dead = trtCurrentTime() + SECONDS2TICKS(0.2);
 	    trtSleepUntil(rel, dead);
 	}
 }
 
 // --- define task 2  ----------------------------------------
-void IR_lock(void* args) {
+void IR_lock() {
 	uint32_t rel, dead;
 	char ir_tx_data[buffer_size];
 	char ir_rx_data[buffer_size];
+	char rec_state;
 	// Initialize the state variable
 	lock_state = IDLE;
+	// Initialize the state of the lock
+	state = LOCK;
 	// Read the switches 
 	read_switches();
 	// FSM for IR_key
 	while (TRUE) {
+		/*
 		// Check that the function is either KEY or BOTH
-		if (function == LOCK) {
+		if ((function == fLOCK) | (function == fBOTH)) {
 			switch (lock_state) {
 				case IDLE:
 					trtWait(SEM_RX_WAIT);
-					char rec_state = ir_rec_packet('K', ir_rx_data);
+					rec_state = ir_rec_packet('K', ir_rx_data);
 					trtSignal(SEM_RX_WAIT);
 					if (rec_state == 0) {
 						state = CHALLENGE;
@@ -191,170 +180,48 @@ void IR_lock(void* args) {
 					break;
 				case RESPONSE:
 					trtWait(SEM_RX_WAIT);
-					char rec_state = ir_rec_packet('K', ir_rx_data);
+					rec_state = ir_rec_packet('K', ir_rx_data);
 					trtSignal(SEM_RX_WAIT);
 					// Transmission correctly recieved. 
 					if (rec_state == 0) {
 						char op = ir_rx_data[0];
 						char auth = ir_rx_data[1];
-						char[] data = ir_rx_data+2;
-						if (trtCurrentTime() - atol(data) <= 1000 & auth == '>') {
-							// DO SOMETHING HERE!
-							trtWait(SEM_TX_WAIT) ; // wait for the message to be received
-							Message_vin = vin ;
-							trtSignal(SEM_RX_WAIT) ; // tell receiver messsage is ready
-						};
+						char* data = ir_rx_data+2;
+						if ((trtCurrentTime() - atol(data) <= 1000) & (auth == '>')) {
+							// Change LED state. Unlocked = light. Locked = no light
+							if (((state == LOCK) & (op == UNLOCK)) | ((state == UNLOCK) & (op == LOCK))) {
+								state = (op == UNLOCK)? UNLOCK : LOCK;
+								PORTC = PORTC ^ 0x01 ;
+							}
+						}
+						op = (op == LOCK) ? 'L' : 'U';
+						auth = (auth == AUTHORIZED) ? 'Y' : 'N';
+						trtWait(SEM_TX_WAIT) ; // wait for the message to be received
+						fprintf(stdout, op + ' ' + auth + ' ' + data + '\n' + '\r');
+						trtSignal(SEM_RX_WAIT) ; // tell receiver messsage is ready
 						state = IDLE;
 					}
 					break;
-				case DEFAULT:
+				default:
 					// Something is wrong. Recover to IDLE. 
 					key_state = IDLE;
 			}
 		}
+		*/
 		// Sleep
 	    rel = trtCurrentTime() + SECONDS2TICKS(0.1);
-	    dead = trtCurrentTime() + SECONDS2TICKS(0.1);
+	    dead = trtCurrentTime() + SECONDS2TICKS(0.2);
 	    trtSleepUntil(rel, dead);
 	}
 }
 
-// --- define task 3  ----------------------------------------
-void print1(void* args) {
-    uint32_t rel, dead ;
-	while(TRUE) {
-		// wait for the message to be valid
-		// Using trtAccept means that the task can keep running
-		// if the message is not ready yet
-		if(trtAccept(SEM_RX_WAIT)) {
-			fprintf(stdout,"Input*2= %d \n\r", Message_vin*2) ;
-			trtSignal(SEM_TX_WAIT); 
-		}
-
-		// Sleep
-	    rel = trtCurrentTime() + SECONDS2TICKS(0.2);
-	    dead = trtCurrentTime() + SECONDS2TICKS(0.2);
-	    trtSleepUntil(rel, dead);	
-	}
-}
-
-/* void led1(void* args) 
-  begin
-
-	while(1)
-	begin
-		// wait on semaphore SEM_TASK1_WAIT 
-		// which is signaled from task 2
-		trtWait(SEM_TASK1_WAIT) ; 
-		
-		// blink the led
-		PORTC = PORTC ^ 0x02 ;
-
-		// update task count
-		trtWait(SEM_SHARED) ;
-		task_count++;
-		trtSignal(SEM_SHARED);
-
-	end
-  end 
-
-// --- define task 2  ----------------------------------------
-void led2(void* args) 
-  begin	
-  	uint32_t rel, dead ;
-
-	while(1)
-	begin
-		// If button zero is pushed, signal task1 to execute
-		if (~PINB & (1<<PINB0)) 
-			trtSignal(SEM_TASK1_WAIT) ;
-		
-		// blink the led
-		PORTC = PORTC ^ 0x04 ;
-
-		// update task count
-		trtWait(SEM_SHARED) ;
-		task_count++;
-		trtSignal(SEM_SHARED);
-
-		// Sleep
-	    rel = trtCurrentTime() + SECONDS2TICKS(0.1);
-	    dead = trtCurrentTime() + SECONDS2TICKS(0.1);
-	    trtSleepUntil(rel, dead);
-	end
-  end
-
-
-// --- define task 3  ----------------------------------------
-void print1(void* args) 
-  begin	
-    uint32_t rel, dead ;
-	while(1)
-	begin
-		// blink the led
-		PORTC = PORTC ^ 0x08 ;
-
-		// update task count
-		trtWait(SEM_SHARED) ;
-		task_count++;
-		trtSignal(SEM_SHARED);
-
-		// wait for the message to be valid
-		// Using trtAccept means that the task can keep running
-		// if the message is not ready yet
-		if(trtAccept(SEM_RX_WAIT)) 
-		begin
-			fprintf(stdout,"Input*2= %d \n\r", Message_vin*2) ;
-			trtSignal(SEM_TX_WAIT); 
-		end	
-
-		// Sleep
-	    rel = trtCurrentTime() + SECONDS2TICKS(0.2);
-	    dead = trtCurrentTime() + SECONDS2TICKS(0.2);
-	    trtSleepUntil(rel, dead);	
-	end
-  end
-
-// --- define task 4  ----------------------------------------
-void print2(void* args) 
-  begin
-	uint16_t vin ;
-
-	while(1)
-	begin
-		// blink the led
-		PORTC = PORTC ^ 0x10 ;
-		
-		// message to task3 -- 
-		// need two semaphores
-		trtWait(SEM_TX_WAIT) ; // wait for the message to be received
-		// exercise the uart
-		fprintf(stdout, "enter integer>") ;
-		fscanf(stdin, "%d", &vin) ;
-		//trtWait(SEM_STRING_DONE); // waits for the <enter>
-		fprintf(stdout,"%ld %ld %d\n\r", 
-					trtCurrentTime(), task_count, vin) ;
-		Message_vin = vin ;
-		trtSignal(SEM_RX_WAIT) ; // tell receiver messsage is ready
-
-		// update task count
-		trtWait(SEM_SHARED) ;
-		task_count++ ;
-		trtSignal(SEM_SHARED);
-
-		// kill the process when B.1 is pressed
-		if (~PINB & (1<<PINB1)) 
-			trtTerminate() ;
-	end
-  end
-*/
 // --- Main Program ----------------------------------
 int main(void) {
 
-  DDRC = 0xff;    // led connections
+  DDRC = 0xff;    	// led connections
   PORTC = 0xff;
-  DDRB = 0x00; // switch connections
-  PORTB = 0xff; // pullup on
+  DDRB = 0x00; 		// switch connections
+  PORTB = 0xff; 	// pullup on
 
   //init the UART -- trt_uart_init() is in trtUart.c
   trt_uart_init();
@@ -369,9 +236,6 @@ int main(void) {
   trtCreateSemaphore(SEM_RX_ISR_SIGNAL, 0) ; // uart receive ISR semaphore
   trtCreateSemaphore(SEM_STRING_DONE,0) ;  // user typed <enter>
   
-  // Task synch
-  trtCreateSemaphore(SEM_TASK1_WAIT, 0) ; // task2 controls task1 rate
-  
   // message protection
   trtCreateSemaphore(SEM_TX_WAIT, 1) ; // message send interlock
   trtCreateSemaphore(SEM_RX_WAIT, 0) ; // message receive interlock
@@ -380,19 +244,17 @@ int main(void) {
   trtCreateSemaphore(SEM_SHARED, 1) ; // protect shared variable
 
  // --- creat tasks  ----------------
-  /*
-  trtCreateTask(led1, 100, SECONDS2TICKS(0.05), SECONDS2TICKS(0.05), &(args[0]));
-  trtCreateTask(led2, 100, SECONDS2TICKS(0.1), SECONDS2TICKS(0.1), &(args[1]));
-  trtCreateTask(print1, 100, SECONDS2TICKS(0.1), SECONDS2TICKS(0.1), &(args[2]));
-  trtCreateTask(print2, 100, SECONDS2TICKS(0.1), SECONDS2TICKS(0.1), &(args[2]));
-  */
+  
+  trtCreateTask(IR_lock, 100, SECONDS2TICKS(0.1), SECONDS2TICKS(0.2), &(args[0]));
+  trtCreateTask(IR_key, 100, SECONDS2TICKS(0.1), SECONDS2TICKS(0.2), &(args[1]));
+  
   // --- Idle task --------------------------------------
   // For debugging, blink an LED
   // For production, you would probably comment out the body of the WHILE
-  while (1) 
-  begin
+  while (1) {}
+  /*{
   	PORTC = PORTC ^ 0x80 ;
 	_delay_ms(500) ;
-  end
+  }*/
 
 } // main
