3c1333f03100f2223bc39c039f7742d92f9d4f9a
am838 <am838@cornell.edu>
2013-11-30 14:18:06 -0500

    Feedback loop complete. Needs calibration.

--- Lab5/main.c
+++ Lab5/main.c
@@ -34,10 +34,15 @@ int args[2] ;
 #define FALSE 0
 #define TRUE 1
 
+// relay & LED bits
+#define LED_EN 	 0x04
+#define RELAY_EN 0x08
+
 // input and output variables
 volatile int cTemp;		// current temperature
 volatile int dTemp;		// desired temperature
 volatile int time_rem;  // time remaining in seconds
+volatile int adc_val;	// read adc value
 
 // timer variable
 volatile int msec;
@@ -51,21 +56,6 @@ int8_t lcd_buffer[17];	// LCD display buffer
 // 							ISRs & Helper Functions
 /********************************************************************/
 // --- Timer ISR ------------------------
-/*
-ISR (TIMER2_COMPA_vect) {
-	trtWait(SEM_SHARED);
-	if (time_rem > 0) {
-		if (msec < 1000) {
-			msec++;
-		}
-		else {
-			msec = 0;
-			time_rem--;
-		}
-	}
-    trtSignal(SEM_SHARED);
-}
-*/
 ISR (TIMER0_COMPA_vect) {
 	trtWait(SEM_SHARED);
 	if (time_rem > 0) {
@@ -93,74 +83,66 @@ void init_lcd(void) {
 //********************************************************** 
 // ADC setup
 void adc_init(void){
- ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));    //16Mhz/128 = 125Khz the ADC reference clock
- ADMUX |= (1<<REFS0);                //Voltage reference from Avcc (5v)
- ADCSRA |= (1<<ADEN);                //Turn on ADC
- ADCSRA |= (1<<ADSC);                //Do an initial conversion because this one is the slowest and to ensure that everything is up and running
+	ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));    //16Mhz/128 = 125Khz the ADC reference clock
+	ADMUX |= (1<<REFS0);                //Voltage reference from Avcc (5v)
+	//ADMUX |= (0<<REFS1)
+	ADCSRA |= (1<<ADEN);                //Turn on ADC
+	ADCSRA |= (1<<ADSC);                //Do an initial conversion because this one is the slowest and to ensure that everything is up and running
 }
  
 uint16_t read_adc(uint8_t channel){
- ADMUX &= 0xF0;                    //Clear the older channel that was read
- ADMUX |= channel;                //Defines the new ADC channel to be read
- ADCSRA |= (1<<ADSC);                //Starts a new conversion
- while(ADCSRA & (1<<ADSC));            //Wait until the conversion is done
- return ADCW;                    //Returns the ADC value of the chosen channel
+	ADMUX &= 0xF0;                    //Clear the older channel that was read
+	ADMUX |= channel;                //Defines the new ADC channel to be read
+	ADCSRA |= (1<<ADSC);                //Starts a new conversion
+	while(ADCSRA & (1<<ADSC));            //Wait until the conversion is done
+	return ADCW;                    //Returns the ADC value of the chosen channel
 }
 // http://hekilledmywire.wordpress.com/2011/03/16/using-the-adc-tutorial-part-5/
 
 //********************************************************** 
 //Set it all up
 void initialize(void) {
-  DDRC = 0xff;    	// LCD connections
-  PORTC = 0x00;
-  DDRD |= 0x04;	// LED status light
+	DDRA = 0x00;		// ADC Port
+	PORTA = 0x00;
+	DDRC = 0xff;    	// LCD connections
+	PORTC = 0x00;
+	DDRD = 0x0C;		// LED status light && Relay
+	PORTC = 0x08;		// Initialize relay to high
 
-  //DDRB = 0x00; 		// switch connections
-  //PORTB = 0xff; 	// pullup on
+	// ******************** 
+	//initialize variables
+	trtWait(SEM_SHARED);
+	cTemp = 0.0;
+	dTemp = 0.0;
+	time_rem = 0;
+	msec = 0;
+	trtSignal(SEM_SHARED);
 
-  //******************** 
-  //initialize variables
-  trtWait(SEM_SHARED);
-  cTemp = 0.0;
-  dTemp = 0.0;
-  time_rem = 0;
-  msec = 0;
-  trtSignal(SEM_SHARED);
+	// ******************** 
+	//init LCD
+	init_lcd();
+	LCDclr();
+	// put some stuff on LCD
+	CopyStringtoLCD(LCD_line1, 0, 0);//start at char=0 line=0
+	CopyStringtoLCD(LCD_line2, 0, 1); 
 
-  //******************** 
-  //init LCD
-  init_lcd();
-  LCDclr();
-  // put some stuff on LCD
-  CopyStringtoLCD(LCD_line1, 0, 0);//start at char=0 line=0
-  CopyStringtoLCD(LCD_line2, 0, 1); 
+	// ********************
+	//init ADC
+	adc_init();
 
+	// ******************** 
+	//set up timer 0 for 1 mSec timebase 
+	TIMSK0= (1<<OCIE0A);	//turn on timer 0 cmp match ISR 
+	OCR0A = 249;  		//set the compare reg to 250 time ticks
+	//set prescalar to divide by 64 
+	TCCR0B= 3; 	
+	// turn on clear-on-match
+	TCCR0A= (1<<WGM01) ;
 
-  // ******************** 
-  /*
-  //set up timer 2 for 1 mSec timebase 
-  TIMSK2= (1<<OCIE2A);	//turn on timer 2 cmp match ISR 
-  OCR2A = 249;  		//set the compare reg to 250 time ticks
-  //set prescalar to divide by 64 
-  TCCR2B= 3; 	
-  
-  // turn on clear-on-match
-  TCCR2A= (1<<WGM21) ;
-  */
-
-  //set up timer 0 for 1 mSec timebase 
-  TIMSK0= (1<<OCIE0A);	//turn on timer 0 cmp match ISR 
-  OCR0A = 249;  		//set the compare reg to 250 time ticks
-  //set prescalar to divide by 64 
-  TCCR0B= 3; 	
-  // turn on clear-on-match
-  TCCR0A= (1<<WGM01) ;
-
-  // ********************
-  //crank up the ISRs
-  sei();
+	// ********************
+	//crank up the ISRs
+	sei();
 }  
-//==================================================
 
 /********************************************************************/
 // 							FSM Tasks
@@ -182,11 +164,12 @@ void serialComm(void* args) {
 		trtWait(SEM_SHARED) ;
 		if (strcmp(cmd, "temp") == 0) {
 			dTemp = num;
-			fprintf(stdout, "dTemp: %i\n\r", dTemp); 
+			//volatile int t = adc_val < dTemp;
+			fprintf(stdout, "dTemp: %i, adc: %i\n\r", dTemp, adc_val); 
 		}
 		if (strcmp(cmd, "time") == 0) {
 			time_rem = num;
-			//msec= 0;
+			msec= 0;
 			fprintf(stdout, "Time: %i\n\r", time_rem); 
 		}
 		if (strcmp(cmd, "set") == 0) {
@@ -204,11 +187,11 @@ void lcdComm(void* args) {
 	while (TRUE) {
 	  // display the current temp
 	  trtWait(SEM_SHARED) ;
-	  sprintf(lcd_buffer, "%iF      ", cTemp);
+	  sprintf(lcd_buffer, "%iC      ", cTemp);
 	  LCDGotoXY(9, 0);
 	  LCDstring(lcd_buffer, strlen(lcd_buffer));
 	  // display the desired temp 
-	  //sprintf(lcd_buffer, "%iF", dTemp);
+	  //sprintf(lcd_buffer, "%iC", dTemp);
 	  sprintf(lcd_buffer, "%is      ", time_rem);
 	  LCDGotoXY(9, 1);
 	  LCDstring(lcd_buffer, strlen(lcd_buffer));
@@ -223,28 +206,31 @@ void lcdComm(void* args) {
 // --- define task 3  ----------------------------------------
 void adjustTemp(void* args) {
 	uint32_t rel, dead;
+	uint16_t adc_in;
 	//double inputValue = 0.0;
 	while(TRUE){
-		/*
-		// detection of the fan speed
-		if( flag ) {
-			trtWait(SEM_SHARED);
-			real_speed = cycles_to_rpm((int) motor_period, 15625);
-			flag = FALSE;
-			//fprintf(stdout, "RPM: %f\n", real_speed);
-			trtSignal(SEM_SHARED);
-			pid_ready = FALSE;
-			inputValue = pid_Controller(speed, real_speed, &pid_data);
-			//fprintf(stdout, "RPM: %f,  delta: %f, target: %f, OCR0A: %d \n\r", real_speed,  inputValue, speed, OCR0A);
-			set_input(inputValue);
+		// Read ADC value
+		adc_in = read_adc(0);
+		//fprintf(stdout, "ADC: %i\n\r", adc_in);
 
-			//second pwm
-			OCR0B = 255 - (real_speed / 12);
+		// Control mechanism
+		// If adjusted adc value is lower than desired. 
+		// Set the control bit to off
+		trtWait(SEM_SHARED);
+		adc_val = adc_in;
+		if (adc_val < dTemp) {
+			//fprintf(stdout, "Turning on");
+			PORTD &= ~RELAY_EN;
 		}
-		*/
+		else {
+			//fprintf(stdout, "Turning off");
+			PORTD |= RELAY_EN;
+		}
+		trtSignal(SEM_SHARED);
+
 		// sleep
-	  	rel = trtCurrentTime() + SECONDS2TICKS(0.02);
-	  	dead = trtCurrentTime() + SECONDS2TICKS(0.03);
+	  	rel = trtCurrentTime() + SECONDS2TICKS(0.25);
+	  	dead = trtCurrentTime() + SECONDS2TICKS(0.5);
 	  	trtSleepUntil(rel, dead);	
 	}
 }
@@ -268,8 +254,8 @@ int main(void) {
 
   // --- create tasks  ----------------
   trtCreateTask(serialComm, 1000, SECONDS2TICKS(0.1), SECONDS2TICKS(0.1), &(args[0]));
-  trtCreateTask(lcdComm, 1000, SECONDS2TICKS(0.2), SECONDS2TICKS(0.4), &(args[0]));
-  //trtCreateTask(adjustTemp, 1000, SECONDS2TICKS(0.02), SECONDS2TICKS(0.03), &(args[0]));
+  trtCreateTask(lcdComm, 1000, SECONDS2TICKS(0.25), SECONDS2TICKS(0.5), &(args[0]));
+  trtCreateTask(adjustTemp, 1000, SECONDS2TICKS(0.25), SECONDS2TICKS(0.5), &(args[0]));
 
   // --- Idle task --------------------------------------
   // For debugging, blink an LED
@@ -277,15 +263,12 @@ int main(void) {
   while (1) {
   	// Light turn on when it is heating up. 
   	if (cTemp >= dTemp) {
-		PORTD |= 0x04;
+		PORTD |= LED_EN;
 	}
 	else {
-		PORTD &= 0xFB;
+		PORTD &= ~LED_EN;
+		//PORTD ^= 0x04;
+		//_delay_ms(500);
 	}
-  /*
-  	PORTC = PORTC ^ 0x01 ;
-  	_delay_ms(500) ;	
-  */
   }
-
 } // main
