a5bb1065cf3ee05ab2afe21883f54b36cb2f55db
am838 <am838@cornell.edu>
2013-11-30 19:50:42 -0500

    Feedback loop and minor calibration completed.
    More calibration and testing needed. Next step: attempt to push it to
    100C and boil a 6 minute egg.

--- Lab5/main.c
+++ Lab5/main.c
@@ -42,10 +42,10 @@ int args[2] ;
 volatile int cTemp;		// current temperature
 volatile int dTemp;		// desired temperature
 volatile int time_rem;  // time remaining in seconds
-volatile int adc_val;	// read adc value
 
 // timer variable
 volatile int msec;
+volatile int count_en;
 
 // lcd variables
 const int8_t LCD_line1[] PROGMEM = "Current:\0";
@@ -58,13 +58,16 @@ int8_t lcd_buffer[17];	// LCD display buffer
 // --- Timer ISR ------------------------
 ISR (TIMER0_COMPA_vect) {
 	trtWait(SEM_SHARED);
-	if (time_rem > 0) {
+	if ((time_rem > 0) && count_en) {
 		if (msec < 1000) {
 			msec++;
 		}
 		else {
 			msec = 0;
 			time_rem--;
+			if (time_rem == 0) {
+				count_en = 0;
+			}
 		}
 	}
     trtSignal(SEM_SHARED);
@@ -85,16 +88,15 @@ void init_lcd(void) {
 void adc_init(void){
 	ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));    //16Mhz/128 = 125Khz the ADC reference clock
 	ADMUX |= (1<<REFS0);                //Voltage reference from Avcc (5v)
-	//ADMUX |= (0<<REFS1)
 	ADCSRA |= (1<<ADEN);                //Turn on ADC
 	ADCSRA |= (1<<ADSC);                //Do an initial conversion because this one is the slowest and to ensure that everything is up and running
 }
  
 uint16_t read_adc(uint8_t channel){
-	ADMUX &= 0xF0;                    //Clear the older channel that was read
-	ADMUX |= channel;                //Defines the new ADC channel to be read
-	ADCSRA |= (1<<ADSC);                //Starts a new conversion
-	while(ADCSRA & (1<<ADSC));            //Wait until the conversion is done
+	ADMUX &= 0xF0;                  //Clear the older channel that was read
+	ADMUX |= channel;               //Defines the new ADC channel to be read
+	ADCSRA |= (1<<ADSC);            //Starts a new conversion
+	while(ADCSRA & (1<<ADSC));     	//Wait until the conversion is done
 	return ADCW;                    //Returns the ADC value of the chosen channel
 }
 // http://hekilledmywire.wordpress.com/2011/03/16/using-the-adc-tutorial-part-5/
@@ -106,16 +108,17 @@ void initialize(void) {
 	PORTA = 0x00;
 	DDRC = 0xff;    	// LCD connections
 	PORTC = 0x00;
-	DDRD = 0x0C;		// LED status light && Relay
-	PORTC = 0x08;		// Initialize relay to high
+	DDRD |= 0x0C;		// LED status light && Relay
+	PORTC |= 0x08;		// Initialize relay to high
 
 	// ******************** 
 	//initialize variables
 	trtWait(SEM_SHARED);
-	cTemp = 0.0;
-	dTemp = 0.0;
+	cTemp = 0;
+	dTemp = 0;
 	time_rem = 0;
 	msec = 0;
+	count_en = 0;
 	trtSignal(SEM_SHARED);
 
 	// ******************** 
@@ -163,18 +166,26 @@ void serialComm(void* args) {
 		// update shared variables
 		trtWait(SEM_SHARED) ;
 		if (strcmp(cmd, "temp") == 0) {
-			dTemp = num;
-			//volatile int t = adc_val < dTemp;
-			fprintf(stdout, "dTemp: %i, adc: %i\n\r", dTemp, adc_val); 
+			if (num < 0) {
+				fprintf(stdout, "Please input a positive temperature value.");
+			}
+			else {
+				dTemp = num;
+			}
 		}
 		if (strcmp(cmd, "time") == 0) {
-			time_rem = num;
-			msec= 0;
-			fprintf(stdout, "Time: %i\n\r", time_rem); 
+			if (num < 0) {
+				fprintf(stdout, "Please input a positive time value in seconds.");
+			}
+			else {
+				time_rem = num;
+				msec= 0;
+			}
 		}
-		if (strcmp(cmd, "set") == 0) {
-			cTemp = num;
-			fprintf(stdout, "cTemp: %i\n\r", time_rem); 
+		// 6 Minute Egg Mode
+		if (strcmp(cmd, "egg") == 0) {
+			cTemp = 100;
+			time_rem = 3600;
 		}
 		trtSignal(SEM_SHARED);
 	}
@@ -191,11 +202,11 @@ void lcdComm(void* args) {
 	  LCDGotoXY(9, 0);
 	  LCDstring(lcd_buffer, strlen(lcd_buffer));
 	  // display the desired temp 
-	  //sprintf(lcd_buffer, "%iC", dTemp);
-	  sprintf(lcd_buffer, "%is      ", time_rem);
+	  sprintf(lcd_buffer, "%iC      ", dTemp);
+	  //sprintf(lcd_buffer, "%is      ", time_rem);
 	  LCDGotoXY(9, 1);
 	  LCDstring(lcd_buffer, strlen(lcd_buffer));
-	  trtSignal(SEM_SHARED) ;
+	  trtSignal(SEM_SHARED);
 	  // sleep
 	  rel = trtCurrentTime() + SECONDS2TICKS(0.25);
 	  dead = trtCurrentTime() + SECONDS2TICKS(0.5);
@@ -207,24 +218,23 @@ void lcdComm(void* args) {
 void adjustTemp(void* args) {
 	uint32_t rel, dead;
 	uint16_t adc_in;
-	//double inputValue = 0.0;
 	while(TRUE){
 		// Read ADC value
 		adc_in = read_adc(0);
 		//fprintf(stdout, "ADC: %i\n\r", adc_in);
 
 		// Control mechanism
-		// If adjusted adc value is lower than desired. 
-		// Set the control bit to off
 		trtWait(SEM_SHARED);
-		adc_val = adc_in;
-		if (adc_val < dTemp) {
-			//fprintf(stdout, "Turning on");
-			PORTD &= ~RELAY_EN;
+		cTemp = (adc_in + 3) / 2.1;
+		if (cTemp < 0) cTemp = 0; 
+		if (cTemp < (dTemp * 9)) {	// Factor of .9 to account for carryover effect
+			PORTD &= ~RELAY_EN;		// Turn on heating element
+			PORTD |= LED_EN;    	// Turn on LED
 		}
 		else {
-			//fprintf(stdout, "Turning off");
-			PORTD |= RELAY_EN;
+			PORTD |= RELAY_EN;		// Turn off heating element
+			PORTD &= ~LED_EN;		// Turn off LED
+			count_en = 1;			// Enable timer count down
 		}
 		trtSignal(SEM_SHARED);
 
@@ -240,7 +250,7 @@ int main(void) {
   //init the UART -- trt_uart_init() is in trtUart.c
   trt_uart_init();
   stdout = stdin = stderr = &uart0;
-  fprintf(stdout,"\n\r Welcome to KitchenBot UI \n\r Please input your instructions below\n\r\n\r");
+  fprintf(stdout,"\n\r Welcome to KitchenBot UI \n\r Please input your instructions below\n\r The options are: time, temp, & egg\n\r\n\r");
     // start TRT
   trtInitKernel(80); // 80 bytes for the idle task stack
 
@@ -257,18 +267,8 @@ int main(void) {
   trtCreateTask(lcdComm, 1000, SECONDS2TICKS(0.25), SECONDS2TICKS(0.5), &(args[0]));
   trtCreateTask(adjustTemp, 1000, SECONDS2TICKS(0.25), SECONDS2TICKS(0.5), &(args[0]));
 
-  // --- Idle task --------------------------------------
-  // For debugging, blink an LED
-  // For production, you would probably comment out the body of the WHILE
+  // --- Main Idle task --------------------------------------
   while (1) {
-  	// Light turn on when it is heating up. 
-  	if (cTemp >= dTemp) {
-		PORTD |= LED_EN;
-	}
-	else {
-		PORTD &= ~LED_EN;
-		//PORTD ^= 0x04;
-		//_delay_ms(500);
-	}
+  	// Do nothing
   }
 } // main
